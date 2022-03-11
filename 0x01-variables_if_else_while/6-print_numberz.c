2142310687db8e8d99edda5fc8d188e77778fdb0
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-11 04:02:14 -0800

    All base 10 single numbers by putchar funcion

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i ='0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
