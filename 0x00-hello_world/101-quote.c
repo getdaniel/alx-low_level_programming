8ae58adb44b32f2f91ab9fe118b08499a2ed0993
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-10 23:48:06 -0800

    print with quote to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main-Entry point
+ * main-prints a string with a quote followed by new line, to standard error
  *
  * Return: Always 0
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
