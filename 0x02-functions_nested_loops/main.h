110860265c77052dabccea1cb77e128baf50f32e
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-15 12:46:43 +0300

    header file - main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Daniel Shibabaw
+ * Description: Header file containing declarations for  all functions
+ *              used in the 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+#endif
