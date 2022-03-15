bea252e2e3ebe68c0983a06b94e8c32c68c6694d
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-15 23:38:16 +0300

    adds two numbers and return the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @x: number one
+ * @y: number two
+ *
+ * Return: Always 0 for success
+ */
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
