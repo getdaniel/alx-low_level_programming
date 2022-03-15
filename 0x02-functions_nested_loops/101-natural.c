07ced013eba9d3dc0e007c1b4420e8ad970babd0
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 00:44:54 +0300

    prints the sum of multiple of 2 num

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int n, sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+	}
+	printf("The sum is %d\n", sum);
+
+	return (0);
+}
