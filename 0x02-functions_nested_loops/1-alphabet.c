e5a950be873f9902deef1fadfc9eb4beeb905c9e
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-15 12:56:13 +0300

    prints lowercase letters in acending order

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar('i');
+	_putchar('\n');
+
+	return (0);
+}
