42f158700eac67c9874218a9e89e3ce06c23b0e7
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-15 13:46:13 +0300

    checks the character as an alpha or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isalpha - checks the character for alpha
+ *
+ * @c: a character to be checked
+ *
+ * Return: 0 or 1
+ */
+int isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
