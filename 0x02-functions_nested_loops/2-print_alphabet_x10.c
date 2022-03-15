4fdc78b5aed1b14bdcc79a04bc002d15247d2205
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-15 13:10:04 +0300

    print lowercaase letter array x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar('j');
+		_putchar('\n');
+	}
+}
