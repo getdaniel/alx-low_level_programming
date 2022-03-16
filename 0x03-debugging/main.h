750a63c82ff3f52b85def17a9292ca19cb12611b
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 21:17:43 +0300

    modification and executed file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,7 @@
  * positive_or_negative - prints the number as negative or positive.
  * largest_number - prints the largest of three numbers.
  * print_remaining_days - prints the validity of the year with its day.
+ * convert_day - converts the year to day.
  *
  * @i: number that checked for positive or negative
  *
@@ -13,6 +14,7 @@
  */
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
