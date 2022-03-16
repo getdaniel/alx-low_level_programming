b70c8fcd89563eec5f34bd32b76dd8b1f95a81e6
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 20:14:14 +0300

    fix some problem by adding * at comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,7 +15,7 @@ int main(void)
 
 	while (i < 10)
 	{
-		putchar(i); /* at this point is infinite */
+		putchar(i); /** at this point is infinite */
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
