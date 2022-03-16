6f5473a6fa2db8fe3a0c7f2805d497991a1878f5
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 20:08:41 +0300

    update by reducing a prototypes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,6 +10,5 @@
  * largest_number - prints the largest of a, b, c
  */
 void positive_or_negative(int i);
-int largest_number(int a, int, int);
-void print_remaining_days(int, int, int);
+
 #endif /* MAIN_H */
