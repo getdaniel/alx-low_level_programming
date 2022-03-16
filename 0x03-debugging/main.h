4cd0cfbc0011425a6bf317fcb91b0c146e7e5b8f
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 12:03:45 +0300

    update to add prototype

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/**
+ * positive_or_negative - prints the number as negative or positive.
+ *
+ * @i: number that checked for positive or negative
+ *
+ * largest_number - prints the largest of a, b, c
+ */
 void positive_or_negative(int i);
-
+int largest_number(int a, int, int);
+void print_remaining_days(int, int, int);
 #endif /* MAIN_H */
