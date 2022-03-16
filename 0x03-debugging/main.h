bae25add08ddc73f4ef3aafed556d1e133cb9eb2
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 20:34:47 +0300

    adding prototype largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,11 +4,13 @@
 #include <stdio.h>
 /**
  * positive_or_negative - prints the number as negative or positive.
+ * largest_number - prints the largest of three numbers.
  *
  * @i: number that checked for positive or negative
  *
  * largest_number - prints the largest of a, b, c
  */
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
