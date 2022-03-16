7e5ddd28b24225ab0960d87e6b9ae1ab41f476e5
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 12:12:08 +0300

    to fix some problem

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,13 @@ int largest_number(int a, int b, int c)
     {
         largest = b;
     }
+    else if (c > a && c > b)
+    {
+	    largest = c
+    }
     else
     {
-        largest = c;
+        printf("They are equal");
     }
 
     return (largest);
