282a875b540f9a11b9d32671d9eed4e58bb6de52
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-16 21:11:37 +0300

    update to add remaining day prototype

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@
 /**
  * positive_or_negative - prints the number as negative or positive.
  * largest_number - prints the largest of three numbers.
+ * print_remaining_days - prints the validity of the year with its day.
  *
  * @i: number that checked for positive or negative
  *
@@ -12,5 +13,6 @@
  */
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
