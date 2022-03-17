f384c40ebd873a075bf6fcf9ee60a076d9db80b6
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-17 09:50:55 +0300

    main header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - act like putchar
+ * _isupper - to know the case as upper
+ *
+ * @ch: character to be checked.
+ *
+ * Return: depend on the functions work
+ */
+
+char _putchar(char ch);
+int _isupper(int c);
+
+#endif
