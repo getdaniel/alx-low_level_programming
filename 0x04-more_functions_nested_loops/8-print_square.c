ef650b3e948630c04613ca1d2493c88a23e84f56
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-17 11:56:30 +0300

    prints a square shape with #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints square asterix
+ *
+ * @size: the size of square to be printed
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putcahr('#');
+
+			_putchar('\n');
+		}
+	}
+}
