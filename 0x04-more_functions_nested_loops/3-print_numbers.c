51c1ac5b5d1f5a1b3b7fecb7a6ab72556a27a298
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-17 10:34:18 +0300

    prints a number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all digits
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar('i');
+
+	putchar('\n')
+}
