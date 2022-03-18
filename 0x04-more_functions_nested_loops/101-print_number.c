f250f83a2a37cf59e2fbc0f402962bb95c1857cd
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-18 22:09:12 +0300

    fix some problems

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,13 +7,16 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	int INT_MIN = -2147483647 - 1;
+	int INT_MAX = 2147483647;
+
+	if (n < 0 && (n > INT_MIN && n < INT_MAX))
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10)
+	if (n / 10 && (n > INT_MIN && n < INT_MAX))
 		print_number(n / 10);
 
 	_putchar(n % 10 + '0');
