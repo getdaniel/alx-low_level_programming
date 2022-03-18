43de3dcc267dcfc6b7146b17f2774758fa6e5203
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-18 21:00:31 +0300

    fix some bug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -30,8 +30,8 @@ void print_remaining_days(int month, int day, int year)
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 
-		else if (month == 2 && day == 60 &&
-                                (year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
+		else if ((month == 2 && day == 60) &&
+				(year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
