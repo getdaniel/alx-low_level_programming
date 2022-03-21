ab640d2c266ab4d974dac184951993b3d5729b54
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-21 21:48:57 +0300

    puts even place character start from 0

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "amin.h"
+
+/**
+ * puts2 - prints a character every one jump
+ *
+ * @str: string to be evaluated.
+ *
+ * Return: jumped character
+ */
+void puts2(cahr *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+
+	_putchar('\0');
+}
