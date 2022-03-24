0b78d327ac65539594fe398ea4d671c7a1efb1da
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-24 17:40:32 +0300

    fixing code of declaration function

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /**
  * *infinite_add - adds two numbers
  *
