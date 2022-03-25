bcc67bfb865b16622be8dc78b6cc0e6ae3e14b03
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-25 20:48:37 +0300

    fixing for max and min of integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,17 +7,18 @@
  */
 void print_number(int n)
 {
-	int INT_MIN = -2147483647;
-	int INT_MAX = 2147483647;
+	unsigned int x;
 
-	if (n < 0 && (n >= INT_MIN && n <= INT_MAX))
+	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
 	}
 
-	if (n / 10 && (n >= INT_MIN && n <= INT_MAX))
-		print_number(n / 10);
+	x = n;
 
-	_putchar(n % 10 + '0');
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
