10f78cc2119a45054f93e05db2709824cec26752
ShagizMan <danielgetaneh2011@gmail.com>
2022-03-30 21:34:20 +0300

    compare two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+char *move(char *);
+int bandersnatch(char *, char *);
+/**
+ * wildcmp - compares two string, check for wildcards expanision
+ *
+ * @s1: one of the string
+ * @s2: other string to be compared with s1
+ *
+ * Return: 1 if the two identical 0 if otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int sum = 0;
+
+	if (*s1 == '\0' && *s2 == '*' && !move(s2))
+		return (1);
+
+	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
+
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+
+	if (*s2 == '*')
+	{
+		s2 = move(s2);
+
+		if (*s2 == '\0')
+			return (1);
+
+		if (*s1 == *s2)
+			sum += wildcmp(s1 + 1, s2 + 1);
+
+		sum += bandersnatch(s1 + 1, s2);
+
+		return (!!sum);
+	}
+
+	return (0);
+}
+
+/**
+ * move - moves the current char past the *
+ *
+ * @s2: string to iterate over
+ *
+ * Return: the address of the character after the *
+ */
+char *move(char *s2)
+{
+	if (*s2 == '*')
+		return (move(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * bandersnatch - checks recursivly for all the paths when
+ *                the charcters are equal
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return value of wildcmp() or of itself
+ */
+int bandersnatch(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1, s2));
+
+	return (bandersnatch(s1 + 1, s2));
+}
