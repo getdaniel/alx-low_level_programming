500d327974169565cfb96b9db1d39234bace9426
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-01 19:51:55 +0300

    fixing errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0(success)
  */
-int main(int argc_attribute_((UNUSED)), char **argv)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", *argv);
 
