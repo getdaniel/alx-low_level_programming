f858ce416ccc0dab40a586ab2c577e29553ece12
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-11 23:42:02 +0300

    frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ *
+ * @d: the dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
