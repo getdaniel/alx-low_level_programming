229723248831665595298b3f75fc561a9a24839d
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-13 23:45:42 +0300

    fixing by adding std library and * to name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
  */
-void print_name(char name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
