4539a19613b5fa5b52d14ca7441fa3a4d8373c3a
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-14 22:47:56 +0300

    syntax correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (index = 0; index < n; index++)
 	{
-		str = va_args(strings, char);
+		str = va_arg(strings, char *);
 
 		if (str == NULL)
 			printf("(nil)");
