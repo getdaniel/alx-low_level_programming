c2628c6e7dbe4eb34e1b7a2735b23cc4e96ce1c5
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-22 01:26:25 +0300

    change %u to %lu

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%lu] %s\n", h->len, h->str);
 
 		h = h->next;
 		nodes++;
