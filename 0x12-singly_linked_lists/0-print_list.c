3e25ca55086e2536249a6c28f9c929328c019a76
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-22 01:29:21 +0300

    add NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,10 +14,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%lu] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		nodes++;
