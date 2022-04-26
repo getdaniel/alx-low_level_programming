1b77c5a1002b454e4b226ec1775731830f648555
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-26 23:37:15 +0300

    fix spacing

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,13 +16,13 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		differ = head - head->next;
 		node++;
-		printf("[%p] %d\n", (void *)head, head->n);
+		printf("[%p]%d\n", (void *)head, head->n);
 
 		if (differ > 0)
 			head = head->next;
 		else
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("->[%p]%d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
