48fca44e52144fe8b0b9d230214a080b4a505215
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-29 11:47:06 +0300

    recode to fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,22 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int index, count = 0;
-	unsigned long int current;
-
-	for (index = 63; index >= 0; index--)
-	{
-		current = n >> index;
-
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-
-	if (!count)
-		_putchar('0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
