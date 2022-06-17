ec43d551d12921d8f6af7b595386506b6986b60b
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-17 14:28:47 +0300

    fix the code

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ShagizMan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp/ https://github.com/ShagizMan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
