1bfbb584c38d70523c21717760966cc2c0e006e2
Daniel Getaneh <74586716+ShagizMan@users.noreply.github.com>
2022-06-30 14:58:05 -0700

    Add files via upload
     >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ *
+ * @ht: The hash table you wnat to add/update the key/value.
+ * @key: The key - can't be an empty string.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, idx;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (idx = index; ht->array[idx]; idx++)
+	{
+		if (strcmp(ht->array[idx]->key, key) == 0)
+		{
+			free(ht->array[idx]->value);
+			ht->array[idx]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
