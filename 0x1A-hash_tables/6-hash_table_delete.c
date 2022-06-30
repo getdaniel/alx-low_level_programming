cbc04bf8b85884ba44ab57b854078a02cc72b0de
Daniel Getaneh <74586716+ShagizMan@users.noreply.github.com>
2022-06-30 15:00:28 -0700

    Add files via upload
     >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ *
+ * @ht: The hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int index;
+
+	for (index = 0; index < ht->size; index)
+	{
+		if (ht->array[index] != NULL)
+			node = ht->array[index];
+
+		while (node != NULL)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+
+	free(head-array);
+	free(head);
+}
