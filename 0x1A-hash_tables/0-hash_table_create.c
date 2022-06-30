2da778b496a3fa0e9e8fb67f0468c14cc4bcd651
Daniel Getaneh <74586716+ShagizMan@users.noreply.github.com>
2022-06-30 14:53:23 -0700

    Add files via upload
    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a hash table.
+ *
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL>
+ * 	   Otherwise - a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsighed long int index;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (index = 0; index < size; index++)
+		ht->array[index] = NULL;
+
+	return (ht);
+}
