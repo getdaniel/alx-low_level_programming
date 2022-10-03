65a12048ee74eab4628a9b0e57ee86ad3f33e0b9
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-03 11:25:20 +0300

    jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,12 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+
 
 #endif
